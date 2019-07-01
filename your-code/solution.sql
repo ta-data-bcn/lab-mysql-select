9f006d9520b6964180c1f185d06b48f563a952c8
Alberto Abreu <abreualberto91@gmail.com>
2019-07-01 19:08:01 +0200

    Almost done except replacing NULL in challenge 4

---
+++ your-code/solution.sql
@@ -0,0 +1,63 @@
+-- Challenge 1
+
+SELECT a.au_id AS AUTHOR_ID, au_lname AS LAST_NAME, au_fname AS FIRST_NAME, title as TITLE, pub_name AS PUBLISHER
+FROM authors AS a
+JOIN titleauthor AS t2
+ON a.au_id = t2.au_id
+
+JOIN titles AS t1
+ON t2.title_id = t1.title_id
+
+JOIN publishers AS p
+ON t1.pub_id = p.pub_id
+;
+
+
+-- Challenge 2
+
+SELECT a.au_id AS AUTHOR_ID, au_lname AS LAST_NAME, au_fname AS FIRST_NAME, pub_name AS PUBLISHER,  COUNT(title) as TITLE_COUNT
+FROM authors AS a
+JOIN titleauthor AS t2
+ON a.au_id = t2.au_id
+
+JOIN titles AS t1
+ON t2.title_id = t1.title_id
+
+JOIN publishers AS p
+ON t1.pub_id = p.pub_id
+
+GROUP BY PUBLISHER, AUTHOR_ID
+ORDER BY  TITLE_COUNT DESC
+;
+
+-- Challenge 3
+
+SELECT a.au_id AS AUTHOR_ID, au_lname AS LAST_NAME, au_fname AS FIRST_NAME, SUM(qty) AS TOTAL
+FROM authors AS a
+JOIN titleauthor AS t2
+ON a.au_id = t2.au_id
+
+JOIN titles AS t1
+ON t2.title_id = t1.title_id
+
+JOIN sales AS s
+ON t1.title_id = s.title_id
+
+GROUP BY AUTHOR_ID
+ORDER BY TOTAL DESC
+LIMIT 3
+;
+
+-- Challenge 4
+
+SELECT a.au_id AS AUTHOR_ID, au_lname AS LAST_NAME, au_fname AS FIRST_NAME, SUM(qty) AS TOTAL
+FROM publications.authors as a
+LEFT JOIN titleauthor as t1
+ON a.au_id = t1.au_id
+
+LEFT JOIN sales AS s
+ON t1.title_id = s.title_id
+
+GROUP BY AUTHOR_ID
+ORDER BY TOTAL DESC
+;
