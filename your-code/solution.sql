2511f6fd927c3ee7f089708259b472869f53ff5a
PauFL <pauferrilloveras@gmail.com>
2019-07-08 20:06:57 +0200

    finished

--- your-code/solution.sql
+++ your-code/solution.sql
@@ -1,3 +1,4 @@
+-- 1 Challenge
 SELECT
 	authors.au_id AS 'AUTHOR_ID',
     authors.au_lname AS 'LAST_NAME',
@@ -9,3 +10,47 @@ FROM authors
 JOIN titleauthor ON authors.au_id = titleauthor.au_id
 JOIN titles ON titleauthor.title_id = titles.title_id
 JOIN publishers ON titles.pub_id = publishers.pub_id;
+
+-- 2 Challenge
+SELECT
+	authors.au_id AS 'AUTHOR_ID',
+    authors.au_lname AS 'LAST_NAME',
+    authors.au_fname AS 'FIRST_NAME',
+    publishers.pub_name AS 'PUBLISHER',
+    -- titles.title AS 'TITLE',
+    COUNT(titles.title) AS 'title_count'
+    
+FROM authors
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titleauthor.title_id = titles.title_id
+JOIN publishers ON titles.pub_id = publishers.pub_id
+
+GROUP BY authors.au_id, publishers.pub_name;
+
+-- 3 Challenge
+SELECT
+	authors.au_id AS 'AUTHOR_ID',
+    authors.au_lname AS 'LAST_NAME',
+    authors.au_fname AS 'FIRST_NAME',
+    SUM(sales.qty) AS 'QUANTITY'
+
+FROM authors
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN sales ON titleauthor.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC
+LIMIT 3;
+
+-- 4 Challenge
+SELECT
+	authors.au_id AS 'AUTHOR_ID',
+    authors.au_lname AS 'LAST_NAME',
+    authors.au_fname AS 'FIRST_NAME',
+    SUM(sales.qty) AS 'QUANTITY'
+
+FROM authors
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN sales ON titleauthor.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC
+LIMIT 23;
