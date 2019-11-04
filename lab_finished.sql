2f5c3e25109bbfc7d423f741a3dee5be9fd6b0c6
estefaniaespinar <estefaniaespinar.c@gmail.com>
2019-11-04 17:30:15 +0100

    Update lab_finished.sql

--- lab_finished.sql
+++ lab_finished.sql
@@ -1,69 +1,36 @@
-## Challenge 1 - Who Have Published What At Where?
-
 use publications;
 
-SELECT
-		a.au_id, 
-		a.au_lname, 
-		a.au_fname,
-		ti.title,
-		p.pub_name
-
-FROM authors as a 
-JOIN titleauthor as ta on a.au_id = ta.au_id
-JOIN titles as ti on ta.title_id = ti.title_id
-JOIN publishers as p on ti.pub_id = p.pub_id;
+##sales_royalty = titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100
+WITH output as (
+WITH subtable as (
 
 SELECT
-	count(*) as table_count
+ti.title_id,
+ta.au_id,
+ti.price, 
+ti.royalty,
+s.qty,
+ti.price * (ti.royalty/100) * s.qty as sales_royalty,
+ti.advance * (ta.royaltyper/100) as advance_1
 
-FROM authors as a 
-JOIN titleauthor as ta on a.au_id = ta.au_id
-JOIN titles as ti on ta.title_id = ti.title_id
-JOIN publishers as p on ti.pub_id = p.pub_id;
-
-
-## Challenge 2 - Who Have Published How Many At Where?
-
-SELECT
-		a.au_id, 
-		a.au_lname, 
-		a.au_fname,
-		p.pub_name,
-		count(distinct ti.title)
-
-FROM authors as a 
-JOIN titleauthor as t on a.au_id = t.au_id
-JOIN titles as ti on t.title_id = ti.title_id
-JOIN publishers as p on ti.pub_id = p.pub_id
-GROUP BY 1,2,3,4;
-    
-## Challenge 3 - Best Selling Authors
+FROM titles ti
+LEFT JOIN sales s ON s.title_id = ti.title_id
+LEFT JOIN titleauthor ta ON ti.title_id = ta.title_id)
 
 SELECT 
-		a.au_id, 
-		a.au_lname, 
-		a.au_fname,
-		SUM(ti.ytd_sales)
+title_id,
+au_id,
+SUM(sales_royalty) as sum_sales_royalty,
+advance_1
 
-FROM authors as a
-JOIN titleauthor as t on a.au_id = t.au_id
-JOIN titles as ti on t.title_id = ti.title_id
-GROUP BY a.au_id, a.au_lname, a.au_fname
-ORDER BY sum(ti.ytd_sales) DESC
+FROM subtable
+GROUP BY 1,2)
+
+SELECT 
+
+au_id,
+sum_sales_royalty + advance_1 as total_profits
+FROM output
+ORDER BY total_profits DESC
 LIMIT 3;
 
-## Challenge 4 - Best Selling Authors Ranking
-
-SELECT 
-		a.au_id, 
-		a.au_lname, 
-		a.au_fname,
-		COALESCE(sum(ytd_sales),0)
-		
-FROM authors as a
-LEFT JOIN titleauthor as t on a.au_id = t.au_id
-LEFT JOIN titles as ti on t.title_id = ti.title_id
-GROUP BY a.au_id
-ORDER BY sum(ti.ytd_sales) DESC
-;
